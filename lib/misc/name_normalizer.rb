5ac2ab3abe8ac273272b12b986dfb09c8d78dc78
Jeff L <jeff@langrsoft.com>
2021-01-26 14:28:36 -0700

    name normalizer solution

--- lib/misc/name_normalizer.rb
+++ lib/misc/name_normalizer.rb
@@ -1,5 +1,64 @@
 class NameNormalizer
+
   def normalize(name)
-    raise("not implemented yet")
+    return "" if name.empty?
+    parse(name)
+    return name if mononym?
+    return format_duonym if duonym?
+    return format_western_name
+  end
+
+  private
+
+  def format_western_name
+    "#{last}, #{first} #{middle_initials}#{suffix}"
+  end
+
+  def suffix
+    ",#{@suffix}" unless @suffix.blank?
+  end
+
+  def format_duonym
+    "#{last}, #{first}"
+  end
+
+  def duonym?
+    @parts.length === 2
+  end
+
+  def initial(name_part)
+    name_part.length === 1 ? name_part : "#{name_part[0]}."
+  end
+
+  def middle_initial
+    initial(@parts[1])
+  end
+
+  def middle_initials
+    @parts.slice(1..-2)
+      .map { | name | initial(name) }
+      .join(" ")
+  end
+
+  def raise_on_excess_commas(name)
+    raise ArgumentError if name.count(",") > 1
+  end
+
+  def parse(name)
+    raise_on_excess_commas(name)
+    base_name, @suffix = name.split(",")
+    @parts = base_name.split(" ")
+  end
+
+  def first
+    @parts.first
+  end
+
+  def last
+    @parts.last
+  end
+
+  def mononym?
+    @parts.length <= 1
   end
 end
--- spec/lib/misc/basics_spec.rb
+++ spec/lib/misc/basics_spec.rb
@@ -3,31 +3,35 @@ require 'rails_helper'
 REPLACE_THIS_WITH_WHAT_THE_REAL_ANSWER_SHOULD_BE = -1
 
 describe 'RSpec' do
-  xit 'describes basic math' do
-    expect(4 * 8).to eq(REPLACE_THIS_WITH_WHAT_THE_REAL_ANSWER_SHOULD_BE)
+  it 'describes basic math' do
+<<<<<<< Updated upstream
+    expect(4 * 8).to eq(42)
+=======
+    expect(4 * 8).to eq(32)
+>>>>>>> Stashed changes
   end
 
-  xit 'describes appending to a list' do
+  it 'describes appending to a list' do
     numbers = [ 12, 1, 1, 1, 2, 1, 3]
 
     numbers.append(1)
 
-    expect(numbers).to eq(REPLACE_THIS_WITH_WHAT_THE_REAL_ANSWER_SHOULD_BE)
+    expect(numbers).to eq([ 12, 1, 1, 1, 2, 1, 3, 1])
   end
 
-  xit 'describes transform operations for elements of a list' do
+  it 'describes transform operations for elements of a list' do
     numbers = [2, 5, 10, 105]
 
-    result = replace_this_with_appropriate_code(numbers)
+    result = numbers.map { |n| n * 2 }
 
     expect(result).to eq([4, 10, 20, 210])
   end
 
-  xit "describes interesting float point numeric results" do
+  it "describes interesting float point numeric results" do
     result = 0.1 + 0.2
 
     # fix this expectation so that it appropriately verifies the sum
-    expect(result).to eq 0.3
+    expect(result).to be_within(0.001).of(0.3)
   end
 
   # region nothing to change here
