b06ab2000323e6a22052eb51809a1dde3666c078
Mike Gorski <mgorski@merchantsbonding.com>
2021-01-26 15:23:54 -0600

    mob next [ci-skip]

--- lib/misc/portfolio/portfolio.rb
+++ lib/misc/portfolio/portfolio.rb
@@ -1,2 +1,15 @@
 class Portfolio
+  attr_accessor :is_empty
+
+  def initialize
+    @is_empty = true
+  end
+
+  def empty?
+    is_empty
+  end
+
+  def purchase
+    @is_empty = false
+  end
 end
--- spec/lib/misc/portfolio/portfolio_spec.rb
+++ spec/lib/misc/portfolio/portfolio_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
 
 describe Portfolio do
+  it 'is empty when created' do
+    expect(subject.empty?).to be true
+  end
+
+  it 'is not empty when a purchase is made' do
+    subject.purchase
+
+    expect(subject.empty?).to be false
+  end
+
+  it 'is empty when all your por' do
+  end
+
 end
