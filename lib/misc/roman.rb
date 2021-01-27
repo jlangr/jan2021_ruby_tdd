ccd4b09258ab2ec5f6114fd135cf1664a80ef08a
Nate Dalo <ndalo@merchantsbonding.com>
2021-01-27 10:30:00 -0600

    most of the converter

--- lib/misc/roman.rb
+++ lib/misc/roman.rb
@@ -1,3 +1,22 @@
+class Roman
+  def convert(arabic)
+    mapping = {
+      'M' => 1000,
+      'C' => 100,
+      'X' => 10,
+      'I' => 1
+    }
 
-def convert(arabic)
+    roman_numeral = ''
+    while arabic > 0
+      mapping.each do |numeral, base|
+        if arabic >= base
+          roman_numeral += numeral
+          arabic -= base
+          break
+        end
+      end
+    end
+    roman_numeral
+  end
 end
--- spec/lib/misc/roman_spec.rb
+++ spec/lib/misc/roman_spec.rb
@@ -1,7 +1,67 @@
-#require 'rails_helper'
+require 'rails_helper'
 
-describe "Roman" do
-  it "converts arabic numbers to roman numbers" do
-    # expect(convert(1)).to eq "I"
+describe Roman do
+  it "converts 1 to I" do
+    expect(subject.convert(1)).to eq "I"
+  end
+
+  it 'converts 2 to II' do
+    expect(subject.convert(2)).to eq "II"
+  end
+
+  it 'converts 3 to III' do
+    expect(subject.convert(3)).to eq "III"
+  end
+
+  xit 'converts 4 to IV' do
+    expect(subject.convert(4)).to eq "IV"
+  end
+
+  it 'converts 10 to X' do
+    expect(subject.convert(10)).to eq "X"
+  end
+
+  it 'converts 20 to XX' do
+    expect(subject.convert(20)).to eq "XX"
+  end
+
+  it 'converts 30 to XXX' do
+    expect(subject.convert(30)).to eq "XXX"
+  end
+
+  it 'converts 100 to C' do
+    expect(subject.convert(100)).to eq "C"
+  end
+
+  it 'conerts 101 to CI' do
+    expect(subject.convert(101)).to eq "CI"
+  end
+
+  it 'conerts 110 to CX' do
+    expect(subject.convert(110)).to eq "CX"
+  end
+
+  it 'conerts 123 to CXXIII' do
+    expect(subject.convert(123)).to eq "CXXIII"
+  end
+
+  it 'converts 200 to CC' do
+    expect(subject.convert(200)).to eq "CC"
+  end
+
+  it 'converts 300 to CCC' do
+    expect(subject.convert(300)).to eq "CCC"
+  end
+
+  it 'converts 1000 to M' do
+    expect(subject.convert(1000)).to eq "M"
+  end
+
+  it 'converts 2000 to MM' do
+    expect(subject.convert(2000)).to eq "MM"
+  end
+
+  it 'converts 3000 to MMM' do
+    expect(subject.convert(3000)).to eq "MMM"
   end
 end
